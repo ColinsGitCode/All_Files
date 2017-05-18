5caebe4863702ea473c0554f2106f8df3ab05d95
ColinsGitCode <xueguodong2016@gmail.com>
2017-05-18 16:19:03 +0900

    sda

---
+++ LearnCode/Python/PythonCode/DataMing/Matplotlib_Test.py
@@ -0,0 +1,9 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 2*np.pi, 50)
+plt.plot(x, np.sin(x),'r-o', x, np.sin(2*x),'g--')
+#  plt.show()
+plt.savefig('PIC.png')
