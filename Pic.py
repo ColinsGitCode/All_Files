23161f14647aa13441a30af577ccd542f3b12831
ColinsGitCode <xueguodong2016@gmail.com>
2017-05-24 12:21:06 +0900

    2017.5.24_12.21

--- Pic.py
+++ Pic.py
@@ -9,6 +9,7 @@ def bIN(element,lList):
     return False
 
 y = 1
+Y = 0.9950
 lFontSize = [5,5.5,6.5,7.5,8,9,10,10.5,11,12,14,16,18,20,22,24,26,28,36,48.72]
 lTestSize = [-8,9,10,12,14,16,18,20,24,28,32,36,40,44,48,54,60,66,72,80,88,96]
 
@@ -25,6 +26,11 @@ for ele in lTestSize:
 
 for i,txt in enumerate(lTestSize):
     plt.annotate(txt,(lTestSize[i],1.0005))
+
+for e in lFontSize:
+    plt.scatter(e,Y,c='g',marker='+')
+#  for i,txt in enumerate(lFontSize):
+    #  plt.annotate(txt,(lFontSize[i],Y+0.0005))
 #  plt.scatter(1,1)
 #  plt.scatter(2,1)
 #  x = np.linspace(0, 2*np.pi, 50)
