ba5eaa3f311f45a34f1dda021a014d060545d90e
ColinsGitCode <xueguodong2016@gmail.com>
2017-12-23 14:03:18 +0900

    zzzz

---
+++ pythonTest.py
@@ -0,0 +1,20 @@
+import re
+def solve(eq,var='x'):
+  eq1 = eq.replace("=","-(")+")"
+  c = eval(eq1,{var:1j})
+  return -c.real/c.imag
+
+str = "1"
+while(str != "0"):
+    str = input("Input Formula: ")
+    print("The Input Formula is : %s" %str)
+    strInfo1 = re.compile('□')
+    str = strInfo1.sub('x',str)
+    strInfo1 = re.compile('÷')
+    str = strInfo1.sub('/',str)
+    #str.replace('□','x')
+    #str.replace('÷','/')
+    str.replace('＝','=')
+    str.replace('×','*')
+    print("The Transfered Formula is : %s" %str)
+    print(solve(str))
