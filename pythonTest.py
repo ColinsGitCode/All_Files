04b6c668995e1ec363b76eee7e83f6b1737db45b
Colin Xue <xueguodong2016@gmail.com>
2017-12-23 15:21:16 +0900

    xxxx

--- pythonTest.py
+++ pythonTest.py
@@ -5,6 +5,64 @@ def solve(eq,var='x'):
   c = eval(eq1,{var:1j})
   return -c.real/c.imag
 
+index = 0
+
+def expValue(exp):
+    global index
+    result = termValue(exp)
+    more = True
+    while more:
+        #index += 1
+        op = exp[index]
+        if op in '+-':
+            index += 1
+            value = termValue(exp)
+            if op == '+':
+                result += value
+            else:
+                result -= value
+        else:
+            more = False
+    return result
+
+def factorValue(exp):
+    global index
+    result = 0
+    c = exp[index]
+    if c == '(':
+        index += 1
+        result = expValue(exp)
+        index += 1
+    else:
+        while c.isdigit():
+            result = 10 * result + int(c) - 0
+            index += 1
+            c = exp[index]
+    return result
+
+def termValue(exp):
+    global index
+    result = factorValue(exp)
+    while True:
+        op = exp[index]
+        if op in '*/':
+            index += 1
+            value = factorValue(exp)
+            if op == '*':
+                result *= value
+            else:
+                result /= value
+        else:
+            break
+    return result
+
+def EXPVALUE(STR):
+    exp = STR + "#"
+    result = expValue(exp)
+    print('EXP : %d' % result)
+    print('EXP : %d' % result.as_integer_ratio())
+
+
 str = "1"
 while(str != "0"):
     str = input("Input Formula: ")
@@ -21,7 +79,17 @@ while(str != "0"):
     str = strInfo1.sub('+',str)
     strInfo1 = re.compile('－')
     str = strInfo1.sub('-',str)
+    strInfo1 = re.compile('の')
+    str = strInfo1.sub('*',str)
+    strInfo1 = re.compile('％')
+    str = strInfo1.sub('*0.01',str)
     print("The Transfered Formula is : %s" %str)
-    print(solve(str))
-    print(solve(str).as_integer_ratio())
+    if("=" in str):
+        print(solve(str))
+        print(solve(str).as_integer_ratio())
+    else:
+        EXPVALUE(str)
+
+
+
     # print(Fraction(solve(str)))
